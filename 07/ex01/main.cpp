0a932d36d38d645d0ee45835551ed05a1ec3e6c5
david-gloriod <137294724+david-gloriod@users.noreply.github.com>
2023-08-05 22:59:19 +0200

    I think 07 is ok

--- 07/ex01/main.cpp
+++ 07/ex01/main.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
 #include "./template/iter.hpp"
 
-struct PrintElement {
-	void operator()(int& elem) const {
-		std::cout << elem << std::endl;
-	}
-};
+void print(int& elem) {
+	std::cout << elem << std::endl;
+}
 
 int main( void ) {
 	int a[] = {1, 2, 3, 4, 5};
-	::iter(a, 5, PrintElement());
+	::iter(a, 5, print);
 	return 0;
 }
