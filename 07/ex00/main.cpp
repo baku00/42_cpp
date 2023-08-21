34d69f0c620378e9c9b9f2690b12d72790a53ba0
david-gloriod <137294724+david-gloriod@users.noreply.github.com>
2023-08-21 14:34:47 +0200

    07 finish

--- 07/ex00/main.cpp
+++ 07/ex00/main.cpp
@@ -23,11 +23,24 @@ int main(void)
 {
 	Awesome a(2), b(4);
 
-        swap(a, b);
-        std::cout << a << " " << b << std::endl;
-        std::cout << max(a, b) << std::endl;
-        std::cout << min(a, b) << std::endl;
-        return (0);
+	swap(a, b);
+	std::cout << a << " " << b << std::endl;
+	std::cout << max(a, b) << std::endl;
+	std::cout << min(a, b) << std::endl;
+
+	int e = 2;
+	int f = 3;
+	::swap( e, f );
+	std::cout << "e = " << e << ", f = " << f << std::endl;
+	std::cout << "min( e, f ) = " << ::min( e, f ) << std::endl;
+	std::cout << "max( e, f ) = " << ::max( e, f ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	return (0);
 }
 
 // int main( void ) {
--- 07/ex02/main.cpp
+++ 07/ex02/main.cpp
@@ -2,39 +2,44 @@
 #include "Array.hpp"
 
 template<typename T>
-void display(std::string name, Array<T> a)
+void display(std::string name, Array<T> array)
 {
-	std::cout << name << ": " << a << std::endl;
-	for (unsigned int i = 0; i < a.size(); i++)
-		std::cout << name << "[" << i << "]: " << a[i] << std::endl;
+	std::cout << name << ": " << array << std::endl;
+	for (unsigned int i = 0; i < array.size(); i++)
+		std::cout << name << "[" << i << "]: " << array[i] << std::endl;
 	std::cout << std::endl;
 }
 
 int main()
 {
-	std::cout << "Test 0: Création de la liste a et b" << std::endl;
-	Array<int> a(5);
+	std::cout << "Test -1: 42 test" << std::endl;
+	int *a = new int(5);
+
+	std::cout << "a: " << *a << std::endl << std::endl;
+
+	std::cout << "Test 0: Création de la liste z et b" << std::endl;
+	Array<int> z(5);
 	Array<int> b(5);
 
-	for (unsigned int i = 0; i < a.size(); i++)
-		a[i] = i;
+	for (unsigned int i = 0; i < z.size(); i++)
+		z[i] = i;
 	for (unsigned int i = 0; i < b.size(); i++)
 		b[i] = i + 20;
 
 	std::cout << "Default arrays" << std::endl;
-	display("a", a);
+	display("z", z);
 	display("b", b);
 
-	std::cout << std::endl << "Test 1: Copie de a dans b" << std::endl;
-	Array<int> c(a);
+	std::cout << std::endl << "Test 1: Copie de z dans b" << std::endl;
+	Array<int> c(z);
 
 	display("c", c);
 
-	std::cout << std::endl << "Test 2: Edition de a" << std::endl;
-	for (unsigned int i = 0; i < a.size(); i++)
-		a[i] = i * 2;
+	std::cout << std::endl << "Test 2: Edition de z" << std::endl;
+	for (unsigned int i = 0; i < z.size(); i++)
+		z[i] = i * 2;
 
-	display("a", a);
+	display("z", z);
 	display("c", c);
 
 	std::cout << std::endl << "Test 3: Edition de b" << std::endl;
@@ -74,7 +79,7 @@ int main()
 
 	std::cout << std::endl << "Test 9: Index out of range" << std::endl;
 	try {
-		std::cout << "a[10]: " << a[10] << std::endl;
+		std::cout << "z[10]: " << z[10] << std::endl;
 	}
 	catch (std::exception & e) {
 		std::cout << "Exception caught: " << e.what() << std::endl;
